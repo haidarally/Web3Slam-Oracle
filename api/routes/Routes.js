52080ece3e2db7f6936aad03095328de152c1339
Haidar <haidarally@hotmail.com>
2022-06-14 13:22:24 +0400

    commented caching

--- api/routes/Routes.js
+++ api/routes/Routes.js
@@ -2,5 +2,8 @@
 module.exports = function (app) {
   var api = require('../controllers/Controller');
   const apicache = require('apicache-plus');
-  app.route('/data').get(apicache('5 minutes'), api.getData).post(api.addData);
+  app
+    .route('/data')
+    .get(/*apicache('5 minutes'),*/ api.getData)
+    .post(api.addData);
 };
